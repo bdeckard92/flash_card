bce3792d76a7fc47ecfb2743b4a772f614ad6b03
Bob Deckard <bdeckard92@gmail.com>
2017-01-26 12:13:05 -0600

    almost but still not sending data to json

--- flashcard.js
+++ flashcard.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 var json = require("./basic.json")
 
-    console.log(5 + 6);
+console.log(5 + 6);
 
 function Flashcard(front, back) {
     this.front = front;
@@ -15,42 +15,61 @@ Flashcard.prototype.printInfo = function() {
 };
 
 function ClozeCard(visible, invisible) {
-  this.visible = visible;
-  this.invisible = invisible;
+    this.visible = visible;
+    this.invisible = invisible;
 }
 
 
 var askQuestion = function() {
 
-    prompt: "Would you like to make a flashcard?"
+    prompt: "Would you like to make a flashcard?"//this question needs to go outside 
+    //and be separate from the flash card or close maker and then call the proper function based on response.
 
-        if (this.val === yes) {
+        if (this.val === true) {
+        inquirer.prompt([{
+            type: "input",
+
+            message: "What question will go on the front of your card?",
+            name: "front",
+        }, {
+            type: "input",
+            message: "What answer will go on the back of your card",
+            name: "back"
+        }]) elseif(this.val != true) {
             inquirer.prompt([{
-                name: "front",
-                message: "What question will go on the front of your card?"
-            }, {
-                name: "back",
-                message: "What answer will go on the back of your card"
-            }]).then(function(input) {
+                    type: "input",
+                    m essage: "What part of the question would you like to reveal?",
+                    name: "visible",
+                }, {
+                    type: "input",
+                    message: "What part of the question would you like to hide?",
+                    name: "invisible"
+                }
+            }])
+    }.then(function(input) {
 
-                var newCard = new Flashcard(
-                    input.front,
-                    input.back);
-
-
-                newCard.printInfo();
-
-
-                askQuestion();
-            });
-        }
+        var newCard = new Flashcard(
+            input.front,
+            input.back);
 
 
+        newCard.printInfo();
+        fs.writeFile("basic.json", "newCard", function(err, data) {
+            if (err);
+            console.log(err);
+        });
+
+
+        askQuestion();
+    });
+}
 
 
 
 
-        //console.log("A new Flash Card was created");
+
+
+//console.log("A new Flash Card was created");
 
 };
 
